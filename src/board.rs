2277b3a4487cbb2498f30c6ff1c10c6e45984f1f
Doosan <doosan@hartmann-controls.com>
2023-10-07 16:19:49 -0400

    Draw reverse

--- src/board.rs
+++ src/board.rs
@@ -1,6 +1,6 @@
-use std::cell::RefCell;
+use std::{cell::RefCell, borrow::BorrowMut};
 
-use crate::{Square, piece::{Piece, Rook, Knight, Bishop, King, Queen, Pawn}, Color, Point, Type, Props};
+use crate::{Square, piece::{Piece, Rook, Knight, Bishop, King, Queen, Pawn}, Color, Point, Type, Props, error};
 
 
 pub struct Board{
@@ -11,24 +11,24 @@ impl Board{
        Board{ 
         squares : [
         [
-            Square::new(RefCell::new(Some(Box::new(Rook{color:Color::Black,name:Type::Rook,point:Point::new('a',8)}))),Point::new('a',8)),
-            Square::new(RefCell::new(Some(Box::new(Knight{color:Color::Black,name:Type::Knight,point:Point::new('b',8)}))),Point::new('b',8)),
-            Square::new(RefCell::new(Some(Box::new(Bishop{color:Color::Black,name:Type::Bishop,point:Point::new('c',8)}))),Point::new('c',8)),
-            Square::new(RefCell::new(Some(Box::new(King{color:Color::Black,name:Type::King,point:Point::new('d',8)}))),Point::new('d',8)),
-            Square::new(RefCell::new(Some(Box::new(Queen{color:Color::Black,name:Type::Queen,point:Point::new('e',8)}))),Point::new('e',8)),
-            Square::new(RefCell::new(Some(Box::new(Bishop{color:Color::Black,name:Type::Bishop,point:Point::new('f',8)}))),Point::new('f',8)),
-            Square::new(RefCell::new(Some(Box::new(Knight{color:Color::Black,name:Type::Knight,point:Point::new('g',8)}))),Point::new('g',8)),
-            Square::new(RefCell::new(Some(Box::new(Rook{color:Color::Black,name:Type::Rook,point:Point::new('h',8)}))),Point::new('h',8)),
+            Square::new(RefCell::new(Some(Box::new(Rook::new(Color::Black,Type::Rook)))),Point::new('a',8)),
+            Square::new(RefCell::new(Some(Box::new(Knight::new(Color::Black,Type::Knight)))),Point::new('b',8)),
+            Square::new(RefCell::new(Some(Box::new(Bishop::new(Color::Black,Type::Bishop)))),Point::new('c',8)),
+            Square::new(RefCell::new(Some(Box::new(Queen::new(Color::Black,Type::Queen)))),Point::new('d',8)),
+            Square::new(RefCell::new(Some(Box::new(King::new(Color::Black,Type::King)))),Point::new('e',8)),
+            Square::new(RefCell::new(Some(Box::new(Bishop::new(Color::Black,Type::Bishop)))),Point::new('f',8)),
+            Square::new(RefCell::new(Some(Box::new(Knight::new(Color::Black,Type::Knight)))),Point::new('g',8)),
+            Square::new(RefCell::new(Some(Box::new(Rook::new(Color::Black,Type::Rook)))),Point::new('h',8)),
         ],
         [
-            Square::new(RefCell::new(Some(Box::new(Pawn{color:Color::Black,name:Type::Pawn,point:Point::new('a',7)}))),Point::new('a',7)),
-            Square::new(RefCell::new(Some(Box::new(Pawn{color:Color::Black,name:Type::Pawn,point:Point::new('b',7)}))),Point::new('b',7)),
-            Square::new(RefCell::new(Some(Box::new(Pawn{color:Color::Black,name:Type::Pawn,point:Point::new('c',7)}))),Point::new('c',7)),
-            Square::new(RefCell::new(Some(Box::new(Pawn{color:Color::Black,name:Type::Pawn,point:Point::new('d',7)}))),Point::new('d',7)),
-            Square::new(RefCell::new(Some(Box::new(Pawn{color:Color::Black,name:Type::Pawn,point:Point::new('e',7)}))),Point::new('e',7)),
-            Square::new(RefCell::new(Some(Box::new(Pawn{color:Color::Black,name:Type::Pawn,point:Point::new('f',7)}))),Point::new('f',7)),
-            Square::new(RefCell::new(Some(Box::new(Pawn{color:Color::Black,name:Type::Pawn,point:Point::new('g',7)}))),Point::new('g',7)),
-            Square::new(RefCell::new(Some(Box::new(Pawn{color:Color::Black,name:Type::Pawn,point:Point::new('h',7)}))),Point::new('h',7)),
+            Square::new(RefCell::new(Some(Box::new(Pawn::new(Color::Black,Type::Pawn)))),Point::new('a',7)),
+            Square::new(RefCell::new(Some(Box::new(Pawn::new(Color::Black,Type::Pawn)))),Point::new('b',7)),
+            Square::new(RefCell::new(Some(Box::new(Pawn::new(Color::Black,Type::Pawn)))),Point::new('c',7)),
+            Square::new(RefCell::new(Some(Box::new(Pawn::new(Color::Black,Type::Pawn)))),Point::new('d',7)),
+            Square::new(RefCell::new(Some(Box::new(Pawn::new(Color::Black,Type::Pawn)))),Point::new('e',7)),
+            Square::new(RefCell::new(Some(Box::new(Pawn::new(Color::Black,Type::Pawn)))),Point::new('f',7)),
+            Square::new(RefCell::new(Some(Box::new(Pawn::new(Color::Black,Type::Pawn)))),Point::new('g',7)),
+            Square::new(RefCell::new(Some(Box::new(Pawn::new(Color::Black,Type::Pawn)))),Point::new('h',7)),
         ],
         [
             Square::new(RefCell::new(None),Point::new('a',6)),
@@ -71,24 +71,24 @@ impl Board{
             Square::new(RefCell::new(None),Point::new('h',3)),
         ],
         [
-            Square::new(RefCell::new(Some(Box::new(Pawn{color:Color::White,name:Type::Pawn,point:Point::new('a',2)}))),Point::new('a',2)),
-            Square::new(RefCell::new(Some(Box::new(Pawn{color:Color::White,name:Type::Pawn,point:Point::new('b',2)}))),Point::new('b',2)),
-            Square::new(RefCell::new(Some(Box::new(Pawn{color:Color::White,name:Type::Pawn,point:Point::new('c',2)}))),Point::new('c',2)),
-            Square::new(RefCell::new(Some(Box::new(Pawn{color:Color::White,name:Type::Pawn,point:Point::new('d',2)}))),Point::new('d',2)),
-            Square::new(RefCell::new(Some(Box::new(Pawn{color:Color::White,name:Type::Pawn,point:Point::new('e',2)}))),Point::new('e',2)),
-            Square::new(RefCell::new(Some(Box::new(Pawn{color:Color::White,name:Type::Pawn,point:Point::new('f',2)}))),Point::new('f',2)),
-            Square::new(RefCell::new(Some(Box::new(Pawn{color:Color::White,name:Type::Pawn,point:Point::new('g',2)}))),Point::new('g',2)),
-            Square::new(RefCell::new(Some(Box::new(Pawn{color:Color::White,name:Type::Pawn,point:Point::new('h',2)}))),Point::new('h',2)),
+            Square::new(RefCell::new(Some(Box::new(Pawn::new(Color::White,Type::Pawn)))),Point::new('a',2)),
+            Square::new(RefCell::new(Some(Box::new(Pawn::new(Color::White,Type::Pawn)))),Point::new('b',2)),
+            Square::new(RefCell::new(Some(Box::new(Pawn::new(Color::White,Type::Pawn)))),Point::new('c',2)),
+            Square::new(RefCell::new(Some(Box::new(Pawn::new(Color::White,Type::Pawn)))),Point::new('d',2)),
+            Square::new(RefCell::new(Some(Box::new(Pawn::new(Color::White,Type::Pawn)))),Point::new('e',2)),
+            Square::new(RefCell::new(Some(Box::new(Pawn::new(Color::White,Type::Pawn)))),Point::new('f',2)),
+            Square::new(RefCell::new(Some(Box::new(Pawn::new(Color::White,Type::Pawn)))),Point::new('g',2)),
+            Square::new(RefCell::new(Some(Box::new(Pawn::new(Color::White,Type::Pawn)))),Point::new('h',2)),
         ],
         [
-            Square::new(RefCell::new(Some(Box::new(Rook{color:Color::White,name:Type::Rook,point:Point::new('a',1)}))),Point::new('a',1)),
-            Square::new(RefCell::new(Some(Box::new(Knight{color:Color::White,name:Type::Knight,point:Point::new('b',1)}))),Point::new('b',1)),
-            Square::new(RefCell::new(Some(Box::new(Bishop{color:Color::White,name:Type::Bishop,point:Point::new('c',1)}))),Point::new('c',1)),
-            Square::new(RefCell::new(Some(Box::new(King{color:Color::White,name:Type::King,point:Point::new('d',1)}))),Point::new('d',1)),
-            Square::new(RefCell::new(Some(Box::new(Queen{color:Color::White,name:Type::Queen,point:Point::new('e',1)}))),Point::new('e',1)),
-            Square::new(RefCell::new(Some(Box::new(Bishop{color:Color::White,name:Type::Bishop,point:Point::new('f',1)}))),Point::new('f',1)),
-            Square::new(RefCell::new(Some(Box::new(Knight{color:Color::White,name:Type::Knight,point:Point::new('g',1)}))),Point::new('g',1)),
-            Square::new(RefCell::new(Some(Box::new(Rook{color:Color::White,name:Type::Rook,point:Point::new('h',1)}))),Point::new('h',1)),
+            Square::new(RefCell::new(Some(Box::new(Rook::new(Color::White,Type::Rook)))),Point::new('a',1)),
+            Square::new(RefCell::new(Some(Box::new(Knight::new(Color::White,Type::Knight)))),Point::new('b',1)),
+            Square::new(RefCell::new(Some(Box::new(Bishop::new(Color::White,Type::Bishop)))),Point::new('c',1)),
+            Square::new(RefCell::new(Some(Box::new(Queen::new(Color::White,Type::Queen)))),Point::new('d',1)),
+            Square::new(RefCell::new(Some(Box::new(King::new(Color::White,Type::King)))),Point::new('e',1)),
+            Square::new(RefCell::new(Some(Box::new(Bishop::new(Color::White,Type::Bishop)))),Point::new('f',1)),
+            Square::new(RefCell::new(Some(Box::new(Knight::new(Color::White,Type::Knight)))),Point::new('g',1)),
+            Square::new(RefCell::new(Some(Box::new(Rook::new(Color::White,Type::Rook)))),Point::new('h',1)),
         ],
 
     ]
@@ -119,36 +119,94 @@ impl Board{
         piece
     }
 
-    pub fn get_prop(&self, select:&str)->Option<Props>{
+    pub fn get_square(&self, select:&str)->Result<&Square,error::InvalidateInutError>{
         for row in self.squares.iter(){
             for square in row{
-
                 if select.trim()==square.point.notation(){
-                    if square.piece.borrow().is_some(){
-                        return Some(square.piece.borrow().as_ref().unwrap().get_props());
-
-                    } else {
-                        return None;
-                    }
+                    return Ok(&square);
                 }
             }
         }
-        None
+        Err(error::InvalidateInutError)
     }
 
-    pub fn draw(&self){
+    pub fn get_pieces(&self, color:Color)->Vec<&Square>{
+        let mut vec=vec![];
         for row in self.squares.iter(){
             for square in row{
                 let piece= square.piece.borrow();
 
                 if piece.as_ref().is_some(){
-                    let props = piece.as_ref().unwrap().get_props();
-                    print!(" {} ",props.get_name());
-                } else{
-                    print!(" {} ",' ');
+                    if piece.as_ref().unwrap().get_props().color == color{
+                        vec.push(square);
+                    }
                 }
             }
-            println!();
+        }
+        vec
+    }
+
+    pub fn under_move(&self, point:Point, point2:Point)-> bool{
+        for row in self.squares.iter(){
+            for square in row{
+                let piece= square.piece.borrow();
+
+                if piece.as_ref().is_some() && square.point == point{
+                    let x = piece.as_ref().unwrap().tops(point);
+
+
+
+
+                    let moves = piece.as_ref().unwrap().moves(point);
+                    if moves.contains(&point2){
+                        return true;
+                    }else{
+                        return false;
+                    }
+
+                    
+                }
+            }
+        }
+
+        false
+    }
+
+    pub fn is_blocked(&self,point:Point, point2:Point)->bool{
+
+        false
+    }
+
+
+    pub fn draw(&self,color:Color){
+        if color==Color::White{
+            for row in self.squares.iter(){
+                for square in row.iter(){
+                    let piece= square.piece.borrow();
+                    
+                    if piece.as_ref().is_some(){
+                        let props = piece.as_ref().unwrap().get_props();
+                        print!(" {} ",props.get_name());
+                    } else{
+                        print!(" {} ",' ');
+                    }
+                }
+                println!();
+            }
+        } else{
+            for row in self.squares.iter().rev(){
+                for square in row.iter().rev(){
+                    let piece= square.piece.borrow();
+                    
+                    if piece.as_ref().is_some(){
+                        let props = piece.as_ref().unwrap().get_props();
+                        print!(" {} ",props.get_name());
+                    } else{
+                        print!(" {} ",' ');
+                    }
+                }
+                println!();
+            }
         }
     }
     
